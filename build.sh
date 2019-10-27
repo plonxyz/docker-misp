32570585b4c44a0a757f4320ed54c770885fc85d
plonxyz <egon.lampert@gmail.com>
2019-10-27 15:05:13 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@
 docker build \
     --rm=true --force-rm=true \
     --build-arg MYSQL_MISP_PASSWORD=ChangeThisDefaultPassworda9564ebc3289b7a14551baf8ad5ec60a \
-    --build-arg MYSQL_MISP_SERVER=db \
-    --build-arg MISP_FQDN=localhost \
-    --build-arg REDIS_MISP_SERVER=redis \
-    --build-arg MISP_EMAIL=admin@localhost \
+    --build-arg MYSQL_MISP_SERVER=db \ #set FQDN for non-container MYSQL-Server 
+    --build-arg MISP_FQDN=localhost \ # set FDQN for accessing MISP
+    --build-arg REDIS_MISP_SERVER=redis \ # set FQDN for non-container Redis-Server
+    --build-arg MISP_EMAIL=admin@localhost \ # set E-Mail address for MISP admin
     --build-arg MISP_GPG_PASSWORD=ChangeThisDefaultPasswordXuJBao5Q2bps89LWFqWkKgDZwAFpNHvc \
     -t plonxyz/misp_apache:latest \
     $PWD/apache/
